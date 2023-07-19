cdf902a09e640522283df9231cb54c2dfcf7cd82
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-07-19 23:58:24 +0300

    remove books list and init an Flask-SQLAlchemy object

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-
-all_books = []
+app.secret_key = getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book-shelf.db'
+db = SQLAlchemy()
+db.init_app(app=app)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html', books=all_books)
+    return render_template('index.html')
 
 
 @app.route("/add", methods=['POST', 'GET'])
 def add():
     if request.method == 'POST':
         book = {
-            'title' : request.form.get('title'),
+            'title': request.form.get('title'),
             'author': request.form.get('author'),
             'rating': request.form.get('rating')
         }
-        all_books.append(book)
     return render_template('add.html')
 
 
 if __name__ == "__main__":
+    load_dotenv()
     app.run(debug=True)
 
