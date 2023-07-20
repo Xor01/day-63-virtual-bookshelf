3bb8fc1fefc4afb59983feae34f364a56eeb53e4
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-07-20 23:56:46 +0300

    updating editing and creating delete and enhancing the view with bootstrap

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from os import getenv
 from dotenv import load_dotenv
 from book import Book, db
 from book_form import BookForm
+
 app = Flask(__name__)
 app.secret_key = getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book-shelf.db'
@@ -24,6 +25,8 @@ def home():
 @app.route("/add", methods=['POST', 'GET'])
 def add():
     if request.method == 'POST':
+        if request.form.get('cancel'):
+            return redirect(url_for('home'))
         book = {
             'title': request.form.get('title'),
             'author': request.form.get('author'),
@@ -37,14 +40,45 @@ def add():
             return redirect(url_for('home'))
         except Exception as e:
             print(e)
-    return render_template('add.html')
+    form = BookForm()
+    return render_template('add.html', form=form)
 
 
-@app.route('/edit/<int:book_id>')
+@app.route('/edit/<int:book_id>', methods=['GET', 'POST'])
 def edit(book_id):
     book = db.get_or_404(Book, book_id)
     form = BookForm()
+    if request.method == 'POST':
+        try:
+            with app.app_context():
+                book_to_update = db.get_or_404(Book, book_id)
+                book_to_update.title = request.form.get('title')
+                book_to_update.author = request.form.get('author')
+                book_to_update.rating = request.form.get('rating')
+                db.session.commit()
+                return redirect(url_for('home'))
+        except Exception as e:
+            print(e)
     return render_template('edit.html', book=book, form=form)
+
+
+@app.route('/delete/<int:book_id>', methods=['POST', 'GET'])
+def delete(book_id):
+    book = db.get_or_404(Book, book_id)
+    form = BookForm()
+    if request.method == 'POST':
+        if request.form.get('cancel'):
+            return redirect(url_for('home'))
+        if request.form.get('submit'):
+            with app.app_context():
+                try:
+                    book_to_delete = db.get_or_404(Book, book_id)
+                    db.session.delete(book_to_delete)
+                    db.session.commit()
+                    return redirect(url_for('home'))
+                except Exception as e:
+                    print(e)
+    return render_template('delete.html', book=book, form=form)
 
 
 if __name__ == "__main__":
