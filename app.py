542fd4ac9dcd2fc6472d494e4e86bc8e1db7ce02
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-07-20 18:07:32 +0300

    creating a bootstrap form in book_form.py and import it in app.py then display in in edit.html

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap5
 from os import getenv
 from dotenv import load_dotenv
 from book import Book, db
-
+from book_form import BookForm
 app = Flask(__name__)
 app.secret_key = getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book-shelf.db'
 db.init_app(app=app)
+Bootstrap5(app=app)
 
 with app.app_context():
     db.create_all()
@@ -36,6 +38,13 @@ def add():
         except Exception as e:
             print(e)
     return render_template('add.html')
+
+
+@app.route('/edit/<int:book_id>')
+def edit(book_id):
+    book = db.get_or_404(Book, book_id)
+    form = BookForm()
+    return render_template('edit.html', book=book, form=form)
 
 
 if __name__ == "__main__":
