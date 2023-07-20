72ab04297297a06f70c9fe9c47fba74b33ca4dbd
Mohammed A <111096981+Xor01@users.noreply.github.com>
2023-07-20 16:52:09 +0300

    separate the book model from the main app

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 from dotenv import load_dotenv
+from book import Book, db
 
 app = Flask(__name__)
 app.secret_key = getenv('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book-shelf.db'
-db = SQLAlchemy()
 db.init_app(app=app)
-
-
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    author = db.Column(db.String(250), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-
-    def __repr__(self):
-        return f'<Book {self.title}>'
 
 
 @app.route('/')
